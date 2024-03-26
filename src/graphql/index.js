a2c00fbee6ff6d1d42a52839a7fd650a0657a852
toño <caaz160782@gmail.com>
2024-03-25 20:29:31 -0600

    jwt

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,4 +1,3 @@
-// /graphql/index.js
 
 // Dependencia que crea un servidor
 const { ApolloServer } = require('@apollo/server')
@@ -7,34 +6,17 @@ const { ApolloServerPluginLandingPageLocalDefault } = require('@apollo/server/pl
 // Middleware de Express también en @apollo/server
 const { expressMiddleware } = require('@apollo/server/express4')
 const { loadFiles } = require('@graphql-tools/load-files');
+const { buildContext } = require('graphql-passport');
 
-const resolvers = {
-  Query: {
-    hello: () => 'Hola mundo',
-    getPerson: (_, args) => `Hello, my name is ${args.name}, I'm ${args.age} years old!`,
-    getInt: (_, args) => args.age,
-    getFloat: (_, args) => args.price,
-    getString: () => 'palabra',
-    getBoolean: () => true,
-    getID: () => '121212',
-    getNumbers: (_, args) => args.numbers,
-    getProduct: () => {
-      return {
-        id: '1212',
-        name: 'product 1',
-        price: 100.12,
-        description: 'bla bla bla',
-        image: 'http://image.asas',
-        createdAt: new Date().toISOString()
-      }
-    }
-  }
-}
+const resolvers = require('./resolvers');
+
+
 
 const useGraphQL = async (app) => {
   const server = new ApolloServer({
     typeDefs: await loadFiles('./src/**/*.graphql'),
     resolvers,
+    context: ({req, res}) => buildContext({req, res}),
     playground: true,
     plugins: [
       ApolloServerPluginLandingPageLocalDefault
---
+++ src/graphql/resolvers.js
@@ -0,0 +1,28 @@
+const { getProduct, getProducts, addProduct, updateProduct, deleteProduct } = require('./product.resolvers');
+const { login } = require('./auth.resolvers');
+const { addCategory } = require('./category.resolvers');
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hola mundo',
+    getPerson: (_, args) => `Hello, my name is ${args.name}, I'm ${args.age} years old!`,
+    getInt: (_, args) => args.age,
+    getFloat: (_, args) => args.price,
+    getString: () => 'palabra',
+    getBoolean: () => true,
+    getID: () => '121212',
+    getNumbers: (_, args) => args.numbers,
+    // Products
+    product: getProduct,
+    products: getProducts
+  },
+  Mutation: {
+    login,
+    addProduct,
+    updateProduct,
+    deleteProduct,
+    addCategory
+  }
+}
+
+module.exports = resolvers;
